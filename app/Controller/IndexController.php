e0e546db644eb734c79c76e386982f31c715b32c
Loy <loyenrique1@gmail.com>
2015-07-09 20:49:49 -0430

    email fixed, debug removed

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -22,7 +22,7 @@ class IndexController extends AppController {
 			
 			$info = $this->request->data;
 			
-			debug($info);
+			// debug($info);
 			
 			$this->Car->id = $info['Request']['car_id'];
 			$car_info = $this->Car->read();
@@ -32,16 +32,18 @@ class IndexController extends AppController {
 			// sending email with request template
 		
 			$this->Request->save($this->request->data);
-			$this->Session->setFlash('Your request has been sent.');
 			$Email = new CakeEmail();
 			$Email->viewVars(array('info' => $info, 'car_info' => $car_info));
 			$Email->template('request')
 				->emailFormat('html')
 				->to('loyenrique1@gmail.com')
-				->cc(array('loyenrique_1@hotmail.com'))
+				->cc(array('loy_enrique1@hotmail.com'))
 				->from('me@example.com')
 				->subject('Rent Request')
 				->send();
+			
+			$this->Session->setFlash('Your request has been sent.');
+			$this->redirect(array('controller' => 'index', 'action' => 'index'));
 		}
 	
         $this->Car->id = $id;
