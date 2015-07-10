93117f0260b50a3ce4ba9e958f249a755d6211d0
Loy <loyenrique1@gmail.com>
2015-07-09 20:53:57 -0430

    fixes

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -36,7 +36,7 @@ class IndexController extends AppController {
 			$Email->viewVars(array('info' => $info, 'car_info' => $car_info));
 			$Email->template('request')
 				->emailFormat('html')
-				->to('loyenrique1@gmail.com')
+				->to('loyenrique1@gmail.com','loy_enrique1@hotmail.com')
 				->cc(array('loy_enrique1@hotmail.com'))
 				->from('me@example.com')
 				->subject('Rent Request')
